c153fdc1483ed0033aa1f8419ede6bdfdfbd8fb6
yukimasa <ymizuno700705@gmail.com>
2025-03-21 09:30:06 +0900

    v3.3 初期所持金1000hanyを設定し、その金額以上を購入できない機能を実装しました

--- rotogo_VirtualCurrency.rb
+++ rotogo_VirtualCurrency.rb
@@ -1,12 +1,21 @@
 # 仮想通貨を使うためのクラスを定義
 class VirtualCurrency
+  @@poss=1000 # クラス変数として定義
+  
+  # 所持金のゲッター
+  def self.poss
+    @@poss
+  end
+
+  # 購入メソッド
   def self.purchase
     loop do
       print"いくら購入しますか？>>"
       begin
         pur=Integer(gets.chomp)
         # 1円以上の購入を促す
-        raise"自然数を書いてください" if pur<1
+      raise"自然数を書いてください" if pur<1
+      raise"所持金が足りません" if pur>@@poss
         return pur
       rescue ArgumentError
         # 文字、小数を書いた場合のエラー処理
